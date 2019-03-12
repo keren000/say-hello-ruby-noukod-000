a704372b11f49ceb31291a2bde8a1dc2e6944aff
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-12 14:06:23 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,9 @@
 # Build your say_hello method here
+<<<<<<< HEAD
 def say_hello(name="Ruby Programmer")
 puts("Hello #{name}!")
+=======
+def say_hello("Gabriela")
+puts "Hello #{Gabriela}!"
+>>>>>>> dce1d66bec347a04b6a5ab0a2d68f90e22d0d027
 end
